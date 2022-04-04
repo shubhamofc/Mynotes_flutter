fde2acdf9492caebe1fd7a24266b39939880df85
Shubham <skgupta.know@gmail.com>
2022-04-05 01:23:37 +0530

    Day 3 compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-
+import 'pages/home.dart';
+import 'pages/LoginPage.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,10 +8,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  
-  
-     return MaterialApp(
-      home:HomePage(),
+    return  MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+
+      darkTheme: ThemeData(
+        brightness:Brightness.light
+      ) ,
+      routes: {
+        // ignore: non_constant_identifier_names
+        "/": (Context) => const LoginPage(),
+        // ignore: non_constant_identifier_names
+        "/home":(Context) => const HomePage(),
+        // ignore: non_constant_identifier_names
+        "/Login": (Context)=> const LoginPage(),
+      },
     );
   }
-}
+  }
+
